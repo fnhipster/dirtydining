e3be5b583490144a6167404ce7c0f313299d7c86
Carlos A. Cabrera <ccabrera@sapient.com>
2017-08-10 17:35:47 -0400

    A little easter egg

--- native/app/reducers/geolocation.js
+++ native/app/reducers/geolocation.js
@@ -1,7 +1,12 @@
 import { SET_GEOLOCATION } from '../actions/geolocation'
 
-export default (state = null, action) => {
-  switch(action.type) {
+export default (state = {
+  latitude: 25.728156,
+  longitude: -80.24128429999996,
+  latitudeDelta: 0.00922 * 1.5,
+  longitudeDelta: 0.00421 * 1.5
+}, action) => {
+  switch (action.type) {
     case SET_GEOLOCATION:
       return {
         ...state,
