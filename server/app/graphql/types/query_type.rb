f18822e090b51dc15497d793be5dcab086689c76
Andres Garcia <andresgarcia@gmail.com>
2017-09-08 11:52:34 -0400

    Add business name search
    Signed-off-by: Andres Garcia <andresgarcia@gmail.com>

--- server/app/graphql/types/query_type.rb
+++ server/app/graphql/types/query_type.rb
@@ -15,11 +15,19 @@ Types::QueryType = GraphQL::ObjectType.define do
   field :restaurants, !types[Types::RestaurantType] do 
     argument :latitude, !types.Float
     argument :longitude, !types.Float
-    argument :latitudeDelta, !types.Float 
-    argument :longitudeDelta, !types.Float 
-    description "Find Restaurants by Latitude and Longitude"
+    argument :latitudeDelta, types.Float 
+    argument :longitudeDelta, types.Float
+    argument :name, types.String
+    description "Find Restaurants by Latitude,Longitude and Name"
     resolve ->(obj, args, ctx) {
-      results = Restaurant.find_by_geolocation(args[:latitude], args[:longitude], args[:latitudeDelta], args[:longitudeDelta])
+      if args[:latitudeDelta] && args[:longitudeDelta]
+        results = Restaurant.find_by_bounding_box(args[:latitude], args[:longitude], args[:latitudeDelta], args[:longitudeDelta])
+      end
+
+      if args[:name]
+        results = Restaurant.find_around(args[:latitude], args[:longitude], args[:name])
+      end
+
       if results['hits']['total'] > 0
         ids = results['hits']['hits'].pluck("_id")
         Restaurant.where(id: ids).order("field(id, " + ids.join(",") + ")")
--- server/app/models/restaurant.rb
+++ server/app/models/restaurant.rb
@@ -23,16 +23,11 @@ class Restaurant < ApplicationRecord
   	client.index index: 'code-for-miami', type: self.model_name.plural, id: self.id, body: body
   end
 
-  def self.find_by_geolocation(lat, lon, latdelta, londelta, distance = "1km")
+  def self.find_by_bounding_box(lat, lon, latdelta, londelta)
     north_latitude = lat + latdelta / 2
     north_longitude = lon + londelta / 2
     south_latitude = lat - latdelta / 2
     south_longitude = lon - londelta / 2
-
-    logger.info north_latitude
-    logger.info north_longitude
-    logger.info south_latitude
-    logger.info south_longitude
 
     query = Jbuilder.encode do |json|
       json.query do 
@@ -59,4 +54,50 @@ class Restaurant < ApplicationRecord
     client = Elasticsearch::Client.new host: "elasticsearch", log: true
     results = client.search index: 'code-for-miami', type: self.model_name.plural, body: query
   end
+
+  def self.find_around(lat, lon, name = nil, distance = "1km")
+    query = Jbuilder.encode do |json|
+      json.query do 
+        json.bool do
+          json.filter do 
+            json.geo_distance do 
+              json.distance distance
+              json.location do 
+                json.lat lat
+                json.lon lon
+              end
+            end
+          end
+
+          if !name.nil?
+            json.must do 
+              json.array! ['match'] do 
+                json.match do 
+                  json.business_name name
+                end
+              end
+            end
+          end
+        end
+      end 
+      
+      json.sort do 
+        json.array! ['_geo_distance'] do
+          json._geo_distance do 
+            json.location do
+              json.lat lat
+              json.lon lon
+            end
+            
+            json.order "asc"
+            json.unit "km"
+          end
+        end
+      end
+    end
+
+    client = Elasticsearch::Client.new host: "elasticsearch", log: true
+    results = client.search index: 'code-for-miami', type: self.model_name.plural, body: query
+  end
 end
+
